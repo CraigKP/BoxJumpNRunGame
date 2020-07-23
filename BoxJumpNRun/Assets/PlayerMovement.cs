999247401a76f225d0bdea6fe2f6864d1e6d1db0
Craig Pinto <craig-kavan.pinto@zeiss.com>
2020-07-23 14:00:39 +0200

    Box Sliding with some for and user input for left and right (via polling)

---
+++ BoxJumpNRun/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() // Use FixedUpdate when calculating physics. See: https://stackoverflow.com/questions/34447682/what-is-the-difference-between-update-fixedupdate-in-unity 
+    {
+        // Adding a forward force on Z-axis
+        this.rb.AddForce(0, 0, this.forwardForce * Time.deltaTime); // '* Time.deltaTime' is used to make it frame rate invariant.
+
+        if (Input.GetKey("d"))
+        {
+            this.rb.AddForce(this.sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            this.rb.AddForce(-this.sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
