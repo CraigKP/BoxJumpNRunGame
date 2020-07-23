90681299a6c5342e8fbd8d1c8aa2168fa4980d8f
Craig Pinto <craig-kavan.pinto@zeiss.com>
2020-07-23 15:03:11 +0200

    Collisions and player stop on collition

---
+++ BoxJumpNRun/Assets/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+    // This is the default collision method for a object with 'Collider'
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            this.playerMovement.enabled = false;
+        }
+    }
+}
